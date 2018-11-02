b243f848fbfe48346d5ca4e7dd0f086fc760d472
Trevor Larson <lar15009@byui.edu>
2018-11-02 15:52:25 -0600

    Attempting to fix pan zoom for mobile

--- JavaScript/PanControls.js
+++ JavaScript/PanControls.js
@@ -661,10 +661,10 @@ THREE.PanControls = function ( object, domElement ) {
 
 		//console.log( 'handleTouchMoveDollyPan' );
 
-		/*if ( scope.enableZoom ) {
+		if ( scope.enableZoom && event.touches.length === 2) {
 
-			var dx = event.touches[ 0 ].pageX;
-			var dy = event.touches[ 0 ].pageY;
+			var dx = event.touches[ 0 ].pageX - event.touches[ 1 ].pageX;
+			var dy = event.touches[ 0 ].pageY - event.touches[ 1 ].pageY;
 
 			var distance = Math.sqrt( dx * dx + dy * dy );
 
@@ -677,7 +677,7 @@ THREE.PanControls = function ( object, domElement ) {
 			dollyStart.copy( dollyEnd );
 
 		}
-*/
+
 		if ( scope.enablePan ) {
 
 			var x = 0.5 * ( event.touches[ 0 ].pageX);
